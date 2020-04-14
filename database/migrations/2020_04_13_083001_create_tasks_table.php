3653bf53300d424772055511220c7d25625e308c
Николай <47238111+Brontoz554@users.noreply.github.com>
2020-04-14 10:47:54 +0300

    Add files via upload

---
+++ database/migrations/2020_04_13_083001_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('list_id');
+            $table->string('name',40);
+            $table->string('description',100);
+            $table->string('urgency',100);
+            $table->string('mark',100)->default('in process');
+            $table->timestamps();
+
+            $table->foreign('list_id')->references('id')->on('lists')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
