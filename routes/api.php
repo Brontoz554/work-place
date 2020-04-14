9999f4c5293d33521b43219864271d4e84d19dc3
Николай <47238111+Brontoz554@users.noreply.github.com>
2020-04-14 10:45:27 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +12,29 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+//Route::middleware('auth:api')->get('/list/{list}', 'ListCOntroller@show');
+//Route::post('/login', 'UserController@login');
+//Route::middleware('auth:api')::post('/logout', 'UserController@logout');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/user', 'UserController@store');
+Route::get('/user', 'UserController@index');
+Route::get('/user/{user}', 'UserController@show');
+Route::post('/login', 'UserController@login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/list', 'ListController@store');
+    Route::get('/list', 'ListController@index');
+    Route::get('/list/{list}', 'ListController@show');
+    Route::delete('/list/{list}', 'ListController@destroy');
+
+    Route::post('/task', 'TaskController@store');
+    Route::get('/task', 'TaskController@index');
+    Route::put('/task/{task}', 'TaskController@mark');
+    Route::delete('/task/{task}', 'TaskController@destroy');
 });
+
+
